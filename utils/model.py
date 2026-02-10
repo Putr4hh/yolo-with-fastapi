ad6db60ce0f044f29754b93f913c58a0013c88c8
putr4hh <putr4hh@gmail.com>
2026-02-10 14:12:42 +0700

    pp

--- utils/model.py
+++ utils/model.py
@@ -7,28 +7,3 @@ logger = logging.getLogger(__name__)
 class Model:
 
     # Variabel
-    def __init__(self, model_path='yolo26n.pt'):
-        self.model_path = model_path
-        self.model = None
-
-    # Validate
-    def validate(self):
-        
-        # Check is modul avalable?
-        if not os.path.exists(self.model_path):
-            logger.error(f'model path tidak ada {self.model_path}')
-            exit(1)
-        logger.info(f'ada model {self.model_path}')
-
-    # Memuat model
-    def load_model(self):
-
-        # Load model
-        try:
-            logger.info('Pengecekan model')
-            from ultralytics import YOLO
-            self.model = YOLO(self.model_path)
-            logger.info('model memuat berhasil')
-        except Exception as e:
-            logger.error('Error memuat model: {e}')
-            exit(1)
